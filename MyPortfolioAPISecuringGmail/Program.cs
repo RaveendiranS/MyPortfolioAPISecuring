1353e7fcf66011da5d088dc3fe27dd56480a8890
Raveendiran Sakadevan <141387225+RaveendiranS@users.noreply.github.com>
2025-10-27 21:08:42 +0530

    Update Program.cs

--- MyPortfolioAPISecuringGmail/Program.cs
+++ MyPortfolioAPISecuringGmail/Program.cs
@@ -1,30 +1,32 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddHttpClient();
-
 builder.Services.AddControllers();
-//builder.Services.AddHttpClient();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// (Optional) Allow Angular or other clients to call API
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Optional: enable CORS
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
